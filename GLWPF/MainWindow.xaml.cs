e4d37b0bd3c1aa0873d52e56adb64a9db511d7e4
oipi <tuberosumgames@gmail.com>
2025-06-17 23:41:33 +0530

    system apps tracking

--- GLWPF/MainWindow.xaml.cs
+++ GLWPF/MainWindow.xaml.cs
@@ -19,27 +19,85 @@ namespace GLWPF;
 public partial class MainWindow : Window
 {
     private System.Timers.Timer appTimer;
-    private TimeSpan trackedTime = TimeSpan.Zero;
-    private string processName = "notepad";
+
+    // Tracks time each process ID has been alive
+    private Dictionary<int, TimeSpan> processTimes = new();
+
+    // Maps each process ID to its name
+    private Dictionary<int, string> processNames = new();
+
+    // UI elements per process ID
+    private Dictionary<int, TextBlock> processTextBlocks = new();
     public MainWindow()
     {
         InitializeComponent();
-        appTimer = new System.Timers.Timer(1000); // 1 sec
+
+        appTimer = new System.Timers.Timer(1000); // 1 second
         appTimer.Elapsed += OnTimedEvent;
-        appTimer.Start();
+        appTimer.Start(); ;
     }
     private void OnTimedEvent(object? sender, ElapsedEventArgs e)
     {
-        var processes = Process.GetProcessesByName(processName);
-        if (processes.Length > 0)
-        {
-            trackedTime = trackedTime.Add(TimeSpan.FromSeconds(1));
+        var currentProcesses = Process.GetProcesses();
 
-            // Updating UI from timer thread needs dispatcher
+        var activeProcessIds = new HashSet<int>();
+
+        foreach (var proc in currentProcesses)
+        {
+            int pid = proc.Id;
+            string name = proc.ProcessName;
+
+            activeProcessIds.Add(pid);
+
+            if (!processTimes.ContainsKey(pid))
+            {
+                // New process detected
+                processTimes[pid] = TimeSpan.Zero;
+                processNames[pid] = name;
+
+                Dispatcher.Invoke(() =>
+                {
+                    var tb = new TextBlock
+                    {
+                        FontSize = 14,
+                        Text = $"{name} ({pid}): 00:00:00"
+                    };
+                    processTextBlocks[pid] = tb;
+                    AppList.Children.Add(tb);
+                });
+            }
+            else
+            {
+                processTimes[pid] = processTimes[pid].Add(TimeSpan.FromSeconds(1));
+                Dispatcher.Invoke(() =>
+                {
+                    processTextBlocks[pid].Text = $"{processNames[pid]} ({pid}): {processTimes[pid]:hh\\:mm\\:ss}";
+                });
+            }
+        }
+
+        var closedPids = new List<int>();
+        foreach (var pid in processTimes.Keys)
+        {
+            if (!activeProcessIds.Contains(pid))
+                closedPids.Add(pid);
+        }
+
+        foreach (var pid in closedPids)
+        {
+            processTimes.Remove(pid);
+            processNames.Remove(pid);
             Dispatcher.Invoke(() =>
             {
-                TimerText.Text = $"{processName} time: {trackedTime:hh\\:mm\\:ss}";
+                AppList.Children.Remove(processTextBlocks[pid]);
+                processTextBlocks.Remove(pid);
             });
         }
+    }
+    protected override void OnClosed(EventArgs e)
+    {
+        appTimer.Stop();
+        appTimer.Dispose();
+        base.OnClosed(e);
     }
 }
